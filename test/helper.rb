5d9c88366c1af6ab135fc8daf87ef7e26c24f757
rking <rking@panoptic.com>
2012-04-30 00:46:41 -0600

    TODO gems.

--- test/helper.rb
+++ test/helper.rb
@@ -7,8 +7,9 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
-require 'test/unit'
+# require 'rspec'
 require 'shoulda'
+# require 'shoulda-matchers'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))

669ccf9f627cb7226a2510be12b03b2b142c5f35
rking <rking@panoptic.com>
2012-05-01 02:27:12 -0600

    First RSpec

---
+++ spec/basic_spec.rb
@@ -0,0 +1,12 @@
+require 'helper'
+
+describe Pegex::Pegex::Grammar do
+  it "should have a pegexish tree" do
+    gr = described_class.new
+    rules = gr.rules
+    everything = rules.collect do |e| e.keys end.flatten
+    ok_keys = %w(+min +max .sep .any .all .rgx .ref)
+    filtered = everything.select do |e| ok_keys.include? e end
+    everything.should eq filtered
+  end
+end

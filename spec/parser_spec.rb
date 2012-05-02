0fadc96fd264bbb0a85d11e89ebf3a27fb8702e0
rking <rking@panoptic.com>
2012-05-02 02:23:18 -0600

    Pull in grammar.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -13,17 +13,30 @@ describe Pegex::Parser do
   end
 end
 
+
 describe Pegex::Parser do
   before(:each) do
-    @prs = Pegex::Parser.new grammar: {}
+    require 'pegex/pegex/grammar'
+    @prs = Pegex::Parser.new grammar: Pegex::Pegex::Grammar.new
     @input = 'XXX' # XXX
   end
   # TODO:
   # it "should parse correct types of 'input'" do
   #  ...
   # end
-  it 'should require grammar' do
+  it 'should demand grammar' do
     @prs.grammar = nil
     expect { @prs.parse @input }.to raise_error RuntimeError
+  end
+  it "should pull in grammar by string" do
+    prs = described_class.new grammar: 'testgrammar'
+    prs.parse @input
+    prs.grammar.class.should eq Testgrammar
+    prs.camelize('hi_hi/bi').should eq 'HiHi::Bi'
+  end
+  it 'should not alter actual input' do
+    before = @input.clone
+    @prs.parse @input
+    @input.should eq before
   end
 end

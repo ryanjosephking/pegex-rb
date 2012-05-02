abed29702a790d81f8b4edba2f904886ed100eab
rking <rking@panoptic.com>
2012-05-02 02:23:08 -0600

    Don't grab anything automatically.

--- spec/helper.rb
+++ spec/helper.rb
@@ -13,7 +13,4 @@ require 'rspec'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require 'pegex'
-require 'pegex/pegex/grammar'
-
 require 'pp'
